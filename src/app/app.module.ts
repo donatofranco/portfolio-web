e49c81822c761e4017ed7c4c659724cd3f97bd40
Franco Donato <donato.frann@gmail.com>
2021-07-22 13:36:40 -0300

    Se modifican archivos de Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './nav/nav.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FootComponent } from './foot/foot.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PresentacionComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
